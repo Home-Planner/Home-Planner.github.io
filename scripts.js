1a70f929bcf1f9adb92f1c9d2f86d9275c9a0867
Florian APAVOU <florian9303@gmail.com>
2021-12-09 12:32:17 +0100

    Movable Objects

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const furnitures = document.querySelectorAll(".furniture");
+
+furnitures.forEach((furn) => {
+  const drag = (e) => {
+    furn.style.top = e.pageY + "px";
+    furn.style.left = e.pageX + "px";
+    furn.classList.add("redborder");
+  };
+
+ 
+
+  /*function drag(e) {
+
+          }*/
+  furn.addEventListener("mousedown", () => {
+    window.addEventListener("mousemove", drag);
+  });
+
+  window.addEventListener("mouseup", () => {
+    window.removeEventListener("mousemove", drag);
+    furn.classList.remove("redborder");
+  });
+});
